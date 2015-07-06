3addd3e6b9016d306a4523063b9b8b5f424d3704
Evandro Carreira <evandro.carreira@gmail.com>
2015-07-06 16:54:10 -0300

    Dados atuais das feiras de bauru em GeoJSON

---
+++ js/pontos.js
@@ -0,0 +1,701 @@
+var geojsonFeiras = {
+  "type": "FeatureCollection",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "EPSG:3857" 
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Beija-Flor",
+        "endereco" : "Rua Vicente San Roman",
+        "quadras" : "5:6",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Bela Vista",
+        "endereco" : "Rua Silva Jardim",
+        "quadras" : "11",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Bela Vista",
+        "endereco" : "Rua Afonso Simonetti",
+        "quadras" : "12",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Bela Vista",
+        "endereco" : "Rua Alto Acre",
+        "quadras" : "11",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Bela Vista",
+        "endereco" : "Rua Nicola Avalone",
+        "quadras" : "11",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Centro",
+        "endereco" : "Rua Gustavo Maciel",
+        "quadras" : "4:7",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h30"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Centro",
+        "endereco" : "Rua Ezequiel Ramos",
+        "quadras" : "6",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h30"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Centro",
+        "endereco" : "Rua Júlio Prestes",
+        "quadras" : "2",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h30"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FLC",
+        "bairro" : "Gasparini",
+        "endereco" : "Rua dos Gráficos",
+        "quadras" : "3",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Domingo",
+        "tipo" : "FRA",
+        "bairro" : "Jd. Redentor/José Reghino",
+        "endereco" : "Pça. Alcides Pasquareli",
+        "quadras" : "",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "12h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Terça",
+        "tipo" : "FLC",
+        "bairro" : "Altos da Cidade",
+        "endereco" : "Rua Manoel Bento Cruz",
+        "quadras" : "2", 
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Terça",
+        "tipo" : "FLC",
+        "bairro" : "Independência",
+        "endereco" : "Rua Cuba",
+        "quadras" : "10:11",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Terça",
+        "tipo" : "FPR",
+        "bairro" : "Octávio Rasi",
+        "endereco" : "Rua Joaquim Marques de Figueiredo",
+        "quadras" : "12",
+        "horarioInicio" : "16h00",
+        "horarioFim" : "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Terça",
+        "tipo" : "FLC",
+        "bairro" : "Presidente Geisel",
+        "endereco" : "Rua Justino R. Leonardo",
+        "quadras" : "1",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Terça",
+        "tipo" : "FPR",
+        "bairro" : "Vila Aviação",
+        "endereco" : "Pça. Duarte Silva – Rua Gabriel Cara Ruiz",
+        "quadras" : "2",
+        "horarioInicio" : "16h00",
+        "horarioFim" : "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties" : {
+        "diadasemana" : "Terça",
+        "tipo" : "FLC",
+        "bairro" : "Vista Alegre",
+        "endereco" : "Alameda dos Gerânios",
+        "quadras" : "4:5",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FLC",
+        "bairro": "Altos da Cidade" ,
+        "endereco": "Rua Floriano Peixoto"  ,
+        "quadras": "9:10",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FLC",
+        "bairro": "Falcão",
+        "endereco": "Rua Albuquerque Lins",
+        "quadras" : "11",
+        "horarioInicio" : "06h30",
+        "horarioFim": "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FLC",
+        "bairro": "Industrial",
+        "endereco": "Praça Antonio Pedroso",
+        "quadras" : "",
+        "horarioInicio" : "06h30",
+        "horarioFim": "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FLC",
+        "bairro": "Jd. Colina Verde"  ,
+        "endereco": "Rua Lázara Leoni Santana",
+        "quadras" : "S/N",
+        "horarioInicio" : "16h00",
+        "horarioFim": "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FPR",
+        "bairro": "Jd. Nasrala" ,
+        "endereco": "Pça. José dos Santos - Rua Annis Dabus C/ Rua Antônio Alves",
+        "quadras" : "",
+        "horarioInicio" : "16h00",
+        "horarioFim": "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FRA",
+        "bairro": "Jd. Vânia Maria" ,
+        "endereco": "Pça. Nagem dos Santos/R. Carlos Bartalotti",
+        "quadras" : "",
+        "horarioInicio" : "06h30",
+        "horarioFim": "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FLC",
+        "bairro": "Mary Dota" ,
+        "endereco": "Sede da Regional",
+        "quadras" : "",
+        "horarioInicio" : "16h00",
+        "horarioFim": "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quarta",
+        "tipo": "FLC",
+        "bairro": "Nova Esperança",
+        "endereco": "Rua Manoel F. Inojosa",
+        "quadras" : "5",
+        "horarioInicio" : "06h30",
+        "horarioFim": "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quinta",
+        "tipo" : "FLC",
+        "bairro" : "Bela Vista",
+        "endereco" : "Rua Silva Jardim",
+        "quadras" : "14:16 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quinta",
+        "tipo" : "FLC",
+        "bairro" : "Cardia",
+        "endereco" : "Rua Ezequiel Ramos",
+        "quadras" : "12:13 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quinta",
+        "tipo" : "FLC",
+        "bairro" : "Jardim Brasil",
+        "endereco" : "Travessa Nereid Arruda dos Santos",
+        "quadras" : "1 ",
+        "horarioInicio" : "06h00",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quinta",
+        "tipo" : "FLC",
+        "bairro" : "Jardim Estoril",
+        "endereco" : "Praça Nabih Gebara",
+        "quadras" : "",
+        "horarioInicio" : "16h00",
+        "horarioFim" : "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quinta",
+        "tipo" : "FRA",
+        "bairro" : "Mary Dota",
+        "endereco" : "Av. Marcos de Paula Rafael",
+        "quadras" : "19  ",
+        "horarioInicio" : "16h00",
+        "horarioFim" : "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Quinta",
+        "tipo" : "FLC",
+        "bairro" : "Paulistano",
+        "endereco" : "Rua Minas Gerais",
+        "quadras" : "13  ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FLC",
+        "bairro" : "Centro",
+        "endereco" : "Rua Virgílio Malta",
+        "quadras" : "14:15",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FLC",
+        "bairro" : "Jd. Europa",
+        "endereco" : "Rua Clovis Barreto Melchert",
+        "quadras" : "6 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FRA",
+        "bairro" : "Jd. Jacyra / Pq. São Geraldo",
+        "endereco" : "Pça. Getúlio de Oliveira Barreto",
+        "quadras" : "",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FLC",
+        "bairro" : "Presidente Geisel",
+        "endereco" : "Sambódromo",
+        "quadras" : "",
+        "horarioInicio" : "17h00",
+        "horarioFim" : "21h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FLC",
+        "bairro" : "Redentor",
+        "endereco" : "Rua Santa Paula",
+        "quadras" : "3:5",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FLC",
+        "bairro" : "Santa Luzia",
+        "endereco" : "Rua Bauru",
+        "quadras" : "7",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sexta",
+        "tipo" : "FPR/UBA",
+        "bairro" : "Vila Samaritana",
+        "endereco" : "Pça. Portugal - R. Rio Branco",
+        "quadras" : "",
+        "horarioInicio" : "16h00",
+        "horarioFim" : "20h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sábado",
+        "tipo" : "FLC",
+        "bairro" : "Octávio Rasi",
+        "endereco" : "Praça Aristides R. de Morais",
+        "quadras" : "1 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sábado",
+        "tipo" : "FLC",
+        "bairro" : "Pousada da Esperança II",
+        "endereco" : "Rua Pedro de Castro Pereira",
+        "quadras" : "7 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sábado",
+        "tipo" : "FLC",
+        "bairro" : "Seabra",
+        "endereco" : "Rua Marcílio Dias",
+        "quadras" : "5 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sábado",
+        "tipo" : "FLC",
+        "bairro" : "Souto",
+        "endereco" : "Rua Carlos de Campos",
+        "quadras" : "10:14 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sábado",
+        "tipo" : "FRA",
+        "bairro" : "V.industrial/Jd. Marilu",
+        "endereco" : "Rua Romano Luiz Barbugiani",
+        "quadras" : "2 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "diadasemana" : "Sábado",
+        "tipo" : "FRA",
+        "bairro" : "Vila Dutra",
+        "endereco" : "Pça. São Pedro - Rua da Igreja",
+        "quadras" : "1 ",
+        "horarioInicio" : "06h30",
+        "horarioFim" : "11h00"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [[0, 0], [0, 0]]
+      }
+    }
+  ]
+};
