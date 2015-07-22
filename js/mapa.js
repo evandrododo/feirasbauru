9b47e142c85852ce5f4d8ddae8b7b3e02a6ac402
Evandro Carreira <evandro.carreira@gmail.com>
2015-07-22 12:45:56 -0300

    Style de linha

--- js/mapa.js
+++ js/mapa.js
@@ -20,18 +20,17 @@ $(document).ready(function () {
 
 	// Carrega o mapa ~dark~
 	mapa.layerMapa = new ol.layer.Tile({
-		source: new ol.source.OSM()
-		/*
+	//	source: new ol.source.OSM()
+		
 		source: new ol.source.XYZ({
 			attributions: [
 				new ol.Attribution({
 					html: 'Evandro Carreira - Geoprocessamento divertido'
 				}),
-        		ol.source.OSM.ATTRIBUTION
-      		],
-      		url: 'http://s.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
-    	})
-		*/
+        			ol.source.OSM.ATTRIBUTION
+      			],
+      			url: 'http://s.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+    		})
 	});
 
 	// Mostra a posição do mouse
@@ -56,9 +55,58 @@ $(document).ready(function () {
 		view: new ol.View({
 			center: ol.proj.transform([-49.06307, -22.32821], 'EPSG:4326', 'EPSG:3857'),
 			zoom: 15,
-			minZoom: 13,
+//			minZoom: 13,
 			maxZoom: 20
 		})
 	});
+
+
+	/* Plota as feiras */
+	
+	mapa.feirasSource = new ol.source.Vector({
+    		format: new ol.format.GeoJSON(),
+    		loader: function(extent, resolution, projection) {
+      			//monta url com bounding box
+		      	var url = 'pontos.json';
+      				$.ajax({
+        				url: url,
+        				success: function(data) {
+						console.log(data);
+          					var features = (new ol.format.GeoJSON()).readFeatures(data);
+          					mapa.feirasSource.addFeatures(features);
+        				},
+        				beforeSend: function(data) {
+            					mapa.feirasSource.clear();
+        				},
+        				dataType: 'json'
+      				});
+
+    		},
+    		projection: 'EPSG:4326',
+    		strategy: ol.loadingstrategy.bbox
+  	});	
+
+	/* Style dos Pontos */
+  	mapa.stylePlanoFeiras = function(feature, resolution) {
+		var style = [new ol.style.Style({
+        		fill:  new ol.style.Fill({
+          			color: [255,255,255, 0.2]
+        		}),
+        		stroke: new ol.style.Stroke({
+          			color: [255,0,0, 0.8],
+          			width: 6
+        		})
+    		})];
+      		return style;
+  	};
+
+	/* Define os layers com base no source */
+  	mapa.layerFeiras = new ol.layer.Vector({
+    		source: mapa.feirasSource,
+    		name: 'Feiras',
+		style: mapa.stylePlanoFeiras
+  	});
+	
+	mapa.map.addLayer(mapa.layerFeiras);
 
 });
